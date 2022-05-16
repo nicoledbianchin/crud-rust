50d8946e1e2c160c9faf372f05833c71dc389248
nicoledbianchin <nicoledbianchin@gmail.com>
2022-05-16 16:36:57 -0300

    add crud endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
+use rocket_contrib::json::{Json, JsonValue};
 
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+mod band;
+use band::{Band};
+
+#[post("/", data= "<band>")]
+fn create(band: Json<Band>) -> Json<Band> {
+    band
+}
+
+#[get("/")]
+fn read() -> JsonValue {
+json!([
+    "band 1",
+    "band 2"
+])
+}
+
+#[put("/<id>", data= "<band>")]
+fn update(id: i32, band: Json<Band>) -> Json<Band> {
+    band
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> JsonValue {
+    json!({ "status": "ok" })
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/hello", routes![hello])
+        .mount("/band", routes![create, update, delete])
+        .mount("/bands", routes![read])
         .launch();
 }
